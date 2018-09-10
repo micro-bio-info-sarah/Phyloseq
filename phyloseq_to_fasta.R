140c82b6e44a1a554557e7de1a9e50c6eb6cf10f
studsarah <38938582+studsarah@users.noreply.github.com>
2018-09-10 15:05:27 +0200

    Add files via upload

---
+++ phyloseq_to_fasta.R
@@ -0,0 +1,17 @@
+#Sarah HUET, master degree internship (2018) ; huetsarah49@live.fr
+#Script to write a fasta file from a phyloseq object
+
+#PS : phyloseq object 
+
+library(seqinr)
+
+#we extract the taxa_table from the PS
+pstaxa = as.data.frame(tax_table(PS))
+
+#we give a specific id to each taxa
+#here, the id we'll be as : Mx_Genus (for x rows)
+pstaxa$id = paste0("M",1:nrow(pstaxa),"_",pstaxa$Genus)
+#we extract the sequences to a list
+seq = as.list(rownames(pstaxa))
+#we write the fasta file
+write.fasta(seq,pstaxa$id,file.out = "C:/Users/my_folder/my_pstaxa.fasta" )
